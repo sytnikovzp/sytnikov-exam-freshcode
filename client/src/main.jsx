27df81b65e66c8c7858c03d2f754f6c8e5f1fd22
alex <sytnikov.zp@gmail.com>
2024-08-29 11:12:00 +0300

    Removed PWA (serviceWorker) from root 'main' component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,27 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 // =============================================
-import * as serviceWorker from './serviceWorker';
-import store from './store';
 import App from './App';
+import store from './store';
 // =============================================
 import './index.css';
 
-/* ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-); */
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
