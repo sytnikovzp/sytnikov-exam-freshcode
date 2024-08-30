8985357763e41cc4065b2caad2c2bed7ecf9a54e
alex <sytnikov.zp@gmail.com>
2024-08-30 13:52:47 +0300

    Created 'src/app.js'

--- server/index.js
+++ server/index.js
@@ -1,23 +1,13 @@
 const http = require('http');
 // ============================
 require('dotenv').config({ path: '../.env' });
-
-const express = require('express');
-const cors = require('cors');
+// ============================
+const app = require('./src/app');
 require('./dbMongo/mongoose');
-const router = require('./router');
-const controller = require('./socketInit');
-const handlerError = require('./handlerError/handler');
+const controller = require('./src/socketInit');
 
 const HOST = process.env.SERVER_HOST;
 const PORT = process.env.SERVER_PORT || 3000;
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use('/public', express.static('public'));
-app.use(router);
-app.use(handlerError);
 
 const server = http.createServer(app);
 
