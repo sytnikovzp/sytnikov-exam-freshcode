450f732425f7d0a3858015604098d5765d8e617d
alex <sytnikov.zp@gmail.com>
2024-09-07 18:26:24 +0300

    Refactoring contestQueries.js

--- server/src/controllers/queries/contestQueries.js
+++ server/src/controllers/queries/contestQueries.js
@@ -1,67 +1,69 @@
 const createError = require('http-errors');
-const dbPostgres = require('../../db/dbPostgres/models');
+// =============================================
+const { Contest, Offer } = require('../../db/dbPostgres/models');
 
 module.exports.updateContest = async (data, predicate, transaction) => {
-  const [updatedCount, [updatedContest]] = await dbPostgres.Contest.update(
-    data,
-    { where: predicate, returning: true, transaction }
-  );
+  const [updatedCount, [updatedContest]] = await Contest.update(data, {
+    where: predicate,
+    returning: true,
+    transaction,
+  });
 
   if (updatedCount !== 1) {
-    throw createError(500, 'cannot update сontest');
-  } else {
-    return updatedContest.dataValues;
+    throw createError(500, 'Cannot update сontest!');
   }
+
+  return updatedContest.dataValues;
 };
 
 module.exports.updateContestStatus = async (data, predicate, transaction) => {
-  const updateResult = await dbPostgres.Contest.update(data, {
+  const updateResult = await Contest.update(data, {
     where: predicate,
     returning: true,
     transaction,
   });
 
   if (updateResult[0] < 1) {
-    throw createError(500, 'cannot update сontest');
-  } else {
-    return updateResult[1][0].dataValues;
+    throw createError(500, 'Cannot update сontest status!');
   }
+
+  return updateResult[1][0].dataValues;
 };
 
 module.exports.updateOffer = async (data, predicate, transaction) => {
-  const [updatedCount, [updatedOffer]] = await dbPostgres.Offer.update(data, {
+  const [updatedCount, [updatedOffer]] = await Offer.update(data, {
     where: predicate,
     returning: true,
     transaction,
   });
 
   if (updatedCount !== 1) {
-    throw createError(500, 'cannot update offer');
-  } else {
-    return updatedOffer.dataValues;
+    throw createError(500, 'Cannot update offer!');
   }
+
+  return updatedOffer.dataValues;
 };
 
 module.exports.updateOfferStatus = async (data, predicate, transaction) => {
-  const result = await dbPostgres.Offer.update(data, {
+  const result = await Offer.update(data, {
     where: predicate,
     returning: true,
     transaction,
   });
 
   if (result[0] < 1) {
-    throw createError(500, 'cannot update offer');
-  } else {
-    return result[1];
+    throw createError(500, 'Cannot update offer status!');
   }
+
+  return result[1];
 };
 
-module.exports.createOffer = async (data) => {
-  const result = await dbPostgres.Offer.create(data);
+module.exports.createOffer = async (data, transaction) => {
+  const result = await Offer.create(data, { transaction });
 
   if (!result) {
-    throw createError(500, 'cannot create new offer');
-  } else {
-    return result.get({ plain: true });
+    throw createError(500, 'Cannot create new offer!');
   }
+
+  return result.get({ plain: true });
 };
