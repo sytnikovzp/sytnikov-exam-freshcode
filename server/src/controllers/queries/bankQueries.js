00e85fe2800e9ff573d9b2a206a1419abb9b2840
alex <sytnikov.zp@gmail.com>
2024-09-08 22:50:02 +0300

    Refactoring bankQueries

--- server/src/controllers/queries/bankQueries.js
+++ server/src/controllers/queries/bankQueries.js
@@ -1,13 +1,17 @@
 const createError = require('http-errors');
-const dbPostgres = require('../../db/dbPostgres/models');
+// =============================================
+const { Card } = require('../../db/dbPostgres/models');
 
 module.exports.updateBankBalance = async (data, predicate, transaction) => {
-  const [updatedCount, [updatedBank]] = await dbPostgres.Card.update(data, {
+  const [updatedCount, updatedCards] = await Card.update(data, {
     where: predicate,
     returning: true,
     transaction,
   });
+
   if (updatedCount < 2) {
-    throw createError(403, 'Card decline transaction');
+    throw createError(403, 'Card decline transaction!');
   }
+
+  return updatedCards[0];
 };
